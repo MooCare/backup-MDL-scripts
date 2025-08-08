14b93d65bd4f9667f75aed9530b10a4feb9a29c0
Laurent GARNIER <laurent@moocare.fr>
2025-08-08 10:25:13 +0200

    Enhance AWS sync scripts with heartbeat support and improve argument validation

--- aws_sync_moodle.sh
+++ aws_sync_moodle.sh
@@ -5,199 +5,145 @@
 
 #########################################
 # Written by: Laurent Garnier
-# Contact at:
-# Release 1.1
-# Web Page:
-#
+# Contact at: laurent@moocare.fr
+# Date: 2025-08-07
+# Version: 2.0
+# License: GPLv3
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #########################################
 
 #########################################
 # Configuration section
-
-#####
-# Check if at least 3 arguments are provided
-if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
-    echo "Usage: $0 CUSTOMER_NAME DB_NAME DOMAIN_NAME [SUBDOMAIN_NAME]"
+#########################################
+if [ "$#" -lt 4 ] || [ "$#" -gt 5 ]; then
+    echo "Usage: $0 CUSTOMER_NAME DB_NAME DOMAIN_NAME HEARTBEAT [SUBDOMAIN_NAME]"
     exit 1
 fi
 
-# Assign variables from arguments
 CUSTOMER_NAME="$1"
 DB_NAME="$2"
 DOMAIN_NAME="$3"
-SUBDOMAIN_NAME="${4:-}" # Optional: If not provided, defaults to an empty string
+HEARTBEAT="$4"
+SUBDOMAIN_NAME="${5:-}"
 
-#####
-# Get the current date & time
-TIMESTAMP=`date +"%Y-%m-%d_%Hh%M"`
-
-#####
-# Get the date & time for old file to delete
+TIMESTAMP=$(date +"%Y-%m-%d_%Hh%M")
 DELDATE=$(date -d "-1 hour" +"%Y-%m-%d_%Hh%M")
 
-#####
 FILE=$CUSTOMER_NAME"-db-$TIMESTAMP.sql"
 DEL_FILE=$CUSTOMER_NAME"-db-$DELDATE.sql.gz"
 
-####
-# Directory to backup; The directory or directories
-# to be backed up (spacer separeted)
-B_DIRECTORY_DB="/var/backups/aws/db-"$CUSTOMER_NAME
-B_DIRECTORY_DATA="/var/www/vhosts/"$DOMAIN_NAME"/moodledata"
-B_DIRECTORY_DOCS="/var/www/vhosts/"$DOMAIN_NAME"/httpdocs"
-if [ -n "$SUBDOMAIN_NAME" ] && [ "$SUBDOMAIN_NAME" != "0" ]
-then
-	B_DIRECTORY_DOCS="/var/www/vhosts/"$DOMAIN_NAME"/"$SUBDOMAIN_NAME
+B_DIRECTORY_DB="/var/backups/aws/db-$CUSTOMER_NAME"
+B_DIRECTORY_DATA="/var/www/vhosts/$DOMAIN_NAME/moodledata"
+B_DIRECTORY_DOCS="/var/www/vhosts/$DOMAIN_NAME/httpdocs"
+if [ -n "$SUBDOMAIN_NAME" ] && [ "$SUBDOMAIN_NAME" != "0" ]; then
+	B_DIRECTORY_DOCS="/var/www/vhosts/$DOMAIN_NAME/$SUBDOMAIN_NAME"
 fi
-#echo "$B_DIRECTORY_DOCS"
-#exit
 
+S3_BUCKET="s3://$CUSTOMER_NAME/moodle/"
+R_DIRECTORY_DB="${S3_BUCKET}db/"
+R_DIRECTORY_DATA="${S3_BUCKET}moodledata/"
+R_DIRECTORY_DOCS="${S3_BUCKET}httpdocs/"
 
-
-####
-# Remote directory; Put here the directory in the
-# remote server where you can write your backup
-S3_BUCKET="s3://"$CUSTOMER_NAME"/moodle/"
-R_DIRECTORY_DB=$S3_BUCKET"db/"
-R_DIRECTORY_DATA=$S3_BUCKET"moodledata/"
-R_DIRECTORY_DOCS=$S3_BUCKET"httpdocs/"
-
-
-####
-# Admin email; Put here the email of the person who
-# should receive the reports
 ADMIN_EMAIL="contact@moocare.fr"
 
-
-####
-# Command locations; where your commands are, use which
-# command to find them
 AWS="/usr/local/bin/aws"
 MAIL="/usr/bin/mail"
-AWS_SYNC=$AWS" s3 sync --profile "$CUSTOMER_NAME
+AWS_SYNC="$AWS s3 sync --profile $CUSTOMER_NAME"
 
-
-#########################################
-# Program section
-#########################################
 OUTDIR="/tmp"
 EMAIL_FILE="$OUTDIR/email.txt"
 
-
 #########################################
-# DB MOODLE PART
-
-####
-# Make database backup
-mariadb-dump --quick --single-transaction --max-allowed-packet=256M -uadmin -p`cat /etc/psa/.psa.shadow` $DB_NAME > $B_DIRECTORY_DB/$FILE
-
-# Compress the database dump
-gzip $B_DIRECTORY_DB/$FILE
+# DB BACKUP
+#########################################
+mariadb-dump --quick --single-transaction --max-allowed-packet=256M -uadmin -p$(cat /etc/psa/.psa.shadow) "$DB_NAME" > "$B_DIRECTORY_DB/$FILE"
+gzip "$B_DIRECTORY_DB/$FILE"
 COMPRESSED_FILE="$B_DIRECTORY_DB/$FILE.gz"
 
-# Remove the uncompressed file to avoid sending both versions
-if [ -f "$COMPRESSED_FILE" ]; then
-	rm -f $B_DIRECTORY_DB/$FILE
+if [ ! -f "$COMPRESSED_FILE" ]; then
+    echo "Compression failed. Exiting."
+    exit 1
 else
-	echo "Compression failed. Exiting."
-	exit 1
+    rm -f "$B_DIRECTORY_DB/$FILE"
 fi
 
-# Send the archive to the AWS server
-$AWS_SYNC $B_DIRECTORY_DB $R_DIRECTORY_DB --delete
-EXIT_V="$?"
-case $EXIT_V in
+$AWS_SYNC "$B_DIRECTORY_DB" "$R_DIRECTORY_DB" --delete
+EXIT_DB=$?
+case $EXIT_DB in
 	0) O="Success.";;
-	1) O="One or more Amazon S3 transfer operations failed. ";;
+	1) O="One or more Amazon S3 transfer operations failed.";;
 	2) O="Command not parsed or files skipped.";;
-	130) O="Command interruped.";;
+	130) O="Command interrupted.";;
 	252) O="Command syntax invalid.";;
-	253) O="invalid system environment or configuration.";;
+	253) O="Invalid system environment or configuration.";;
 esac
 
-# Send notification on completion
-if [ $EXIT_V != 0 ]
-then
-	touch $EMAIL_FILE
-	echo "Backup result = $O" >> $EMAIL_FILE
-	echo "Date $(date)" >> $EMAIL_FILE
-	$MAIL -s "MOODLE - "$CUSTOMER_NAME" - MOODLE DB - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < $EMAIL_FILE
-	rm -f $EMAIL_FILE
-fi
-
-# On backup success
-if [ $EXIT_V == 0 ]
-then
-	echo "$TIMESTAMP - MOODLE - "$CUSTOMER_NAME" - MOODLE DB - AWS Hourly Sync - succes"
-fi
-
-# On backup success remove one old archive from the server
-if [ $EXIT_V == 0 ]
-then
-	cd $B_DIRECTORY_DB
-	rm $DEL_FILE
-fi
-
-
-#########################################
-# MOODLE DATA PART
-
-# Send the archive to the AWS server
-$AWS_SYNC $B_DIRECTORY_DATA $R_DIRECTORY_DATA --delete --exclude 'cache/*' --exclude 'temp/*' --exclude 'course-backups/*' 
-EXIT_V="$?"
-case $EXIT_V in
-	0) O="Success.";;
-	1) O="One or more Amazon S3 transfer operations failed. ";;
-	2) O="Command not parsed or files skipped.";;
-	130) O="Command interruped.";;
-	252) O="Command syntax invalid.";;
-	253) O="invalid system environment or configuration.";;
-esac
-
-# Send notification on completion
-if [ $EXIT_V != 0 ]
-then
-	touch $EMAIL_FILE
-	echo "Backup result = $O" >> $EMAIL_FILE
-	echo "Date $(date)" >> $EMAIL_FILE
-	$MAIL -s "MOODLE - "$CUSTOMER_NAME" - MOODLEDATA - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < $EMAIL_FILE
-	rm -f $EMAIL_FILE
-fi
-
-# On backup success
-if [ $EXIT_V == 0 ]
-then
-	echo "$TIMESTAMP - MOODLE - "$CUSTOMER_NAME" - MOODLEDATA - AWS Hourly Sync - succes"
+if [ $EXIT_DB != 0 ]; then
+    echo "Backup result = $O" > "$EMAIL_FILE"
+    echo "Date $(date)" >> "$EMAIL_FILE"
+    $MAIL -s "MOODLE - $CUSTOMER_NAME - MOODLE DB - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < "$EMAIL_FILE"
+    rm -f "$EMAIL_FILE"
+else
+    echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - MOODLE DB - AWS Hourly Sync - success"
+    rm -f "$B_DIRECTORY_DB/$DEL_FILE"
 fi
 
 #########################################
-# MOODLE HTTPDOCS PART
-
-# Send the archive to the AWS server
-$AWS_SYNC $B_DIRECTORY_DOCS $R_DIRECTORY_DOCS --delete
-EXIT_V="$?"
-case $EXIT_V in
+# MOODLEDATA BACKUP
+#########################################
+$AWS_SYNC "$B_DIRECTORY_DATA" "$R_DIRECTORY_DATA" --delete --exclude 'cache/*' --exclude 'temp/*' --exclude 'course-backups/*'
+EXIT_DATA=$?
+case $EXIT_DATA in
 	0) O="Success.";;
-	1) O="One or more Amazon S3 transfer operations failed. ";;
+	1) O="One or more Amazon S3 transfer operations failed.";;
 	2) O="Command not parsed or files skipped.";;
-	130) O="Command interruped.";;
+	130) O="Command interrupted.";;
 	252) O="Command syntax invalid.";;
-	253) O="invalid system environment or configuration.";;
+	253) O="Invalid system environment or configuration.";;
 esac
 
-# Send notification on completion
-if [ $EXIT_V != 0 ]
-then
-	touch $EMAIL_FILE
-	echo "Backup result = $O" >> $EMAIL_FILE
-	echo "Date $(date)" >> $EMAIL_FILE
-	$MAIL -s "MOODLE - "$CUSTOMER_NAME" - MOODLE HTTPDOCS - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < $EMAIL_FILE
-	rm -f $EMAIL_FILE
+if [ $EXIT_DATA != 0 ]; then
+    echo "Backup result = $O" > "$EMAIL_FILE"
+    echo "Date $(date)" >> "$EMAIL_FILE"
+    $MAIL -s "MOODLE - $CUSTOMER_NAME - MOODLEDATA - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < "$EMAIL_FILE"
+    rm -f "$EMAIL_FILE"
+else
+    echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - MOODLEDATA - AWS Hourly Sync - success"
 fi
 
-# On backup success
-if [ $EXIT_V == 0 ]
-then
-	echo "$TIMESTAMP - MOODLE - "$CUSTOMER_NAME" - MOODLE HTTPDOCS - AWS Hourly Sync - succes"
+#########################################
+# HTTPDOCS BACKUP
+#########################################
+$AWS_SYNC "$B_DIRECTORY_DOCS" "$R_DIRECTORY_DOCS" --delete
+EXIT_DOCS=$?
+case $EXIT_DOCS in
+	0) O="Success.";;
+	1) O="One or more Amazon S3 transfer operations failed.";;
+	2) O="Command not parsed or files skipped.";;
+	130) O="Command interrupted.";;
+	252) O="Command syntax invalid.";;
+	253) O="Invalid system environment or configuration.";;
+esac
+
+if [ $EXIT_DOCS != 0 ]; then
+    echo "Backup result = $O" > "$EMAIL_FILE"
+    echo "Date $(date)" >> "$EMAIL_FILE"
+    $MAIL -s "MOODLE - $CUSTOMER_NAME - MOODLE HTTPDOCS - $(hostname) AWS Hourly Sync" $ADMIN_EMAIL < "$EMAIL_FILE"
+    rm -f "$EMAIL_FILE"
+else
+    echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - MOODLE HTTPDOCS - AWS Hourly Sync - success"
 fi
 
+#########################################
+# HEARTBEAT if all 3 actions were successful
+#########################################
+if [ $EXIT_DB -eq 0 ] && [ $EXIT_DATA -eq 0 ] && [ $EXIT_DOCS -eq 0 ]; then
+    if [ -n "$HEARTBEAT" ]; then
+        echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - HOURLY HEARTBEAT : $HEARTBEAT"
+        # Uncomment the line below to send the heartbeat ping
+        curl -fsS --retry 3 -X GET $HEARTBEAT > /dev/null
+    fi
+fi
--- aws_sync_moodle_courses.sh
+++ aws_sync_moodle_courses.sh
@@ -5,88 +5,85 @@
 
 #########################################
 # Written by: Laurent Garnier
-# Contact at: 
-# Release 1.1
-# Web Page: 
-# 
+# Contact at: laurent@moocare.fr
+# Date: 2025-08-07
+# Version: 2.0 (with heartbeat support)
+# License: GPLv3
+# This script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #########################################
 
 #########################################
 # Configuration section
+#########################################
 
-#####
 # Check if the required arguments are provided
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 CUSTOMER_NAME DOMAIN_NAME"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 CUSTOMER_NAME DOMAIN_NAME HEARTBEAT"
     exit 1
 fi
 
 # Assign variables from arguments
 CUSTOMER_NAME="$1"
 DOMAIN_NAME="$2"
+HEARTBEAT="$3"
 
+# Directory to backup
+B_DIRECTORY_DATA="/var/www/vhosts/$DOMAIN_NAME/moodledata"
+B_DIRECTORY_COURSES="$B_DIRECTORY_DATA/course-backups"
 
-####
-# Directory to backup; The directory or directories
-# to be backed up (spacer separeted)
-B_DIRECTORY_DATA="/var/www/vhosts/"$DOMAIN_NAME"/moodledata"
-B_DIRECTORY_COURSES=$B_DIRECTORY_DATA"/course-backups"
+# Remote S3 bucket path
+S3_BUCKET="s3://$CUSTOMER_NAME/moodle/"
+R_DIRECTORY_COURSES="${S3_BUCKET}course-backups/"
 
+# Timestamp for logs
+TIMESTAMP=$(date +"%Y-%m-%d_%Hh%M")
 
-####
-# Remote directory; Put here the directory in the
-# remote server where you can write your backup
-S3_BUCKET="s3://"$CUSTOMER_NAME"/moodle/"
-R_DIRECTORY_COURSES=$S3_BUCKET"course-backups/"
-
-#####
-# Get the current date & time
-TIMESTAMP=`date +"%Y-%m-%d_%Hh%M"`
-
-####
-# Admin email; Put here the email of the person who
-# should receive the reports
+# Email for notifications
 ADMIN_EMAIL="contact@moocare.fr"
 
-####
-# Command locations; where your commands are, use which
-# command to find them
+# Paths to commands
 AWS="/usr/local/bin/aws"
 MAIL="/usr/bin/mail"
-AWS_SYNC=$AWS" s3 sync --profile "$CUSTOMER_NAME
+AWS_SYNC="$AWS s3 sync --profile $CUSTOMER_NAME"
 
-
-#########################################
-# Program section
-#########################################
+# Temporary email file
 OUTDIR="/tmp"
 EMAIL_FILE="$OUTDIR/email.txt"
 
-# Send the archive to the AWS server
-$AWS_SYNC $B_DIRECTORY_COURSES $R_DIRECTORY_COURSES --delete
-EXIT_V="$?"
-case $EXIT_V in
+#########################################
+# COURSE BACKUPS SYNC
+#########################################
+$AWS_SYNC "$B_DIRECTORY_COURSES" "$R_DIRECTORY_COURSES" --delete
+EXIT_COURSES=$?
+case $EXIT_COURSES in
 	0) O="Success.";;
-	1) O="One or more Amazon S3 transfer operations failed. ";;
+	1) O="One or more Amazon S3 transfer operations failed.";;
 	2) O="Command not parsed or files skipped.";;
-	130) O="Command interruped.";;
+	130) O="Command interrupted.";;
 	252) O="Command syntax invalid.";;
-	253) O="invalid system environment or configuration.";;
+	253) O="Invalid system environment or configuration.";;
 esac
 
-# Send notification on completion
-if [ $EXIT_V != 0 ]
-then
-	touch $EMAIL_FILE
-	echo "Backup result = $O" >> $EMAIL_FILE
-	echo "Date $(date)" >> $EMAIL_FILE
-	$MAIL -s "MOODLE - "$CUSTOMER_NAME" - COURSES - $(hostname) AWS Daily Sync" $ADMIN_EMAIL < $EMAIL_FILE
-	rm -f $EMAIL_FILE
+# Error notification
+if [ $EXIT_COURSES -ne 0 ]; then
+    echo "Backup result = $O" > "$EMAIL_FILE"
+    echo "Date $(date)" >> "$EMAIL_FILE"
+    $MAIL -s "MOODLE - $CUSTOMER_NAME - COURSES - $(hostname) AWS Daily Sync" $ADMIN_EMAIL < "$EMAIL_FILE"
+    rm -f "$EMAIL_FILE"
+else
+    echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - COURSES - AWS Daily Sync - success"
 fi
 
-# On backup success remove one old archive from the AWS S3 server
-if [ $EXIT_V == 0 ]
-then
-	echo "$TIMESTAMP - MOODLE - "$CUSTOMER_NAME" - COURSES - AWS Daily Sync - succes"
+#########################################
+# HEARTBEAT if success
+#########################################
+if [ $EXIT_COURSES -eq 0 ]; then
+    if [ -n "$HEARTBEAT" ]; then
+        echo "$TIMESTAMP - MOODLE - $CUSTOMER_NAME - DAILY HEARTBEAT : $HEARTBEAT"
+        # Uncomment the line below to send the heartbeat ping
+        curl -fsS --retry 3 -X GET "$HEARTBEAT" > /dev/null
+    fi
 fi
-
